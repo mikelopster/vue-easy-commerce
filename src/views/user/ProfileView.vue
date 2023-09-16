7c4dae4c9d68f02f4d35f1b61fe874b46b934ab4
[ Tanitphon Paniwan ] <lw91945@linecorp.com>
2023-09-16 20:38:27 +0700

    add profile

--- src/views/user/ProfileView.vue
+++ src/views/user/ProfileView.vue
@@ -1,5 +1,48 @@
 <script setup>
-import UserLayout from "@/layouts/UserLayout.vue";
+import { ref, reactive, onMounted } from 'vue'
+import UserLayout from '@/layouts/UserLayout.vue'
+
+import { useEventStore } from '@/stores/event'
+
+const eventStore = useEventStore()
+
+const userForm = [
+  { name: 'Email', field: 'email' },
+  { name: 'Name', field: 'name' }
+]
+
+const userData = reactive({
+  imageUrl: 'https://mikelopster.dev/mikelopster.da6b9a03.webp',
+  email: '',
+  name: ''
+})
+
+onMounted(() => {
+  const savedUserProfile = localStorage.getItem('user-profile')
+  if (savedUserProfile) {
+    const userProfile = JSON.parse(savedUserProfile)
+    userData.imageUrl = userProfile.imageUrl
+    userData.email = userProfile.email
+    userData.name = userProfile.name
+  }
+})
+
+const handleFileChange = (event) => {
+  const file = event.target.files[0]
+
+  if (file) {
+    const reader = new FileReader()
+    reader.onload = (e) => {
+      userData.imageUrl = e.target.result
+    }
+    reader.readAsDataURL(file)
+  }
+}
+
+const updateProfile = () => {
+  localStorage.setItem('user-profile', JSON.stringify(userData))
+  eventStore.popupMessage('success', 'Update Profile successful!')
+}
 </script>
 
 <template>
@@ -11,26 +54,26 @@ import UserLayout from "@/layouts/UserLayout.vue";
       <div class="flex flex-col items-center">
         <div class="avatar">
           <div class="w-24 rounded-full">
-            <img src="https://mikelopster.dev/mikelopster.da6b9a03.webp" />
+            <img :src="userData.imageUrl" />
           </div>
         </div>
 
-        <div v-for="item in [
-          'Email',
-          'Name'
-        ]" class="form-control w-full">
+        <input type="file" @change="handleFileChange">
+
+        <div v-for="item in userForm" class="form-control w-full">
           <label class="label">
-            <span class="label-text">{{ item }}</span>
+            <span class="label-text">{{ item.name }}</span>
             <span class="label-text-alt"></span>
           </label>
           <input
             type="text"
             placeholder="Type here"
             class="input input-bordered w-full"
+            v-model="userData[item.field]"
           />
         </div>
 
-        <button class="btn btn-primary w-full mt-4">Update profile</button>
+        <button class="btn btn-primary w-full mt-4" @click="updateProfile">Update profile</button>
       </div>
     </div>
   </UserLayout>
