39bd14544747d0523ffcbc66c4b12e27e7b3eff3
[ Tanitphon Paniwan ] <lw91945@linecorp.com>
2023-10-01 17:08:37 +0700

    update firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,20 @@
 import { initializeApp } from 'firebase/app'
 import { getFirestore, connectFirestoreEmulator } from 'firebase/firestore/lite'
+import { getAuth, connectAuthEmulator } from 'firebase/auth'
 
 const firebaseConfig = {
   /* your config */
 }
 
 const app = initializeApp(firebaseConfig)
+
 const db = getFirestore(app)
 connectFirestoreEmulator(db, '127.0.0.1', 8080)
 
+const auth = getAuth()
+connectAuthEmulator(auth, 'http://127.0.0.1:9099')
+
 export {
-  db
+  db,
+  auth
 }
